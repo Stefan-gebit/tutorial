7b78a13cec6f8b1efce800b4291513e5ff66691a
Stefan <stefan.lemke@gebit.de>
2016-10-13 15:37:28 +0200

    Comment with Textpad

--- MyGitProject/src/MyGitManager.java
+++ MyGitProject/src/MyGitManager.java
@@ -3,7 +3,7 @@ public class MyGitManager {
 
 	public static void main(String[] args) {
 		System.out.print("first change");
-
+		Person p = new Person();
 	}
 
 }
--- MyGitProject/src/Person.java
+++ MyGitProject/src/Person.java
@@ -4,7 +4,7 @@ public class Person {
 	public void say(String somthing){
 		
 		System.out.println(somthing);
-		Person p = new Person();
+		
 		
 	}
 }
