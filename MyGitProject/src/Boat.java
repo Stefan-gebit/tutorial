766d42e0e0007cd800761196a020adeb96441a7e
Stefan <stefan.lemke@gebit.de>
2016-10-17 09:59:09 +0200

    boat extend with attribut speed

--- MyGitProject/src/Boat.java
+++ MyGitProject/src/Boat.java
@@ -13,7 +13,24 @@ public class Boat {
 	public enum BOATSIZE {LARGE, MEDIUM , SMALL}
 	
 	private Person person;
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
 	private BOATSIZE size;
+	private int speed;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 	public Boat(Person person){
 		this.person = person;
@@ -26,4 +43,6 @@ public class Boat {
 	public BOATSIZE getSize(){
 		return size;		
 	}
+	
+	
 }
