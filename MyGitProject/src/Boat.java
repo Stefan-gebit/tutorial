cbcfffe20bfd3b14d1f70c736f56831bfb94c081
Stefan <stefan.lemke@gebit.de>
2016-10-17 09:41:49 +0200

    new feature to boat added

--- MyGitProject/src/Boat.java
+++ MyGitProject/src/Boat.java
@@ -5,9 +5,19 @@
  *
  */
 public class Boat {
+	public enum BOATSIZE {LARGE, MEDIUM , SMALL}
 	private Person person;
+	private BOATSIZE size;
 
 	public Boat(Person person){
 		this.person = person;
+	}
+	
+	public void setSize(BOATSIZE size){
+		this.size = size;		
+	}
+	
+	public BOATSIZE getSize(){
+		return size;		
 	}
 }
