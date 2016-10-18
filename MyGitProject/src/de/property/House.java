7adea1b989dffa357d476dcb8cbecb54148a17d3
Stefan <stefan.lemke@gebit.de>
2016-10-18 09:39:42 +0200

    change house

--- MyGitProject/src/de/property/House.java
+++ MyGitProject/src/de/property/House.java
@@ -1,10 +1,21 @@
 package de.property;
 
+import de.person.Person;
+
 /**
  * this is a house
  * @author SLemke
  *
  */
 public class House {
+	
+	private Person person;
 
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
 }
