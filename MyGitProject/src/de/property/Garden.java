1fe45df02de9e987e3f5832a0272b5e218840de4
Stefan <stefan.lemke@gebit.de>
2016-10-18 09:17:14 +0200

    some infos

--- MyGitProject/src/de/property/Garden.java
+++ MyGitProject/src/de/property/Garden.java
@@ -1,9 +1,32 @@
 package de.property;
 
+import de.person.Person;
+
+/**
+ * New Feature Garden
+ * 
+ * @author SLemke
+ *
+ */
 public class Garden {
 	
-	public Garden(){
-		super();
+	private int area;
+	private Person person;
+	
+	public Garden(Person person){
+		this.person = person;
 	}
+	
+	public int getArea() {
+		return area;
+	}
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+
+	
+	
+	
 	
 }
